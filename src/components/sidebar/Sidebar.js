bcfd0891dc8728bcaec120392f32a0f2da6e5e4b
=Momen Daoud <=momendaoud07@gamil.com>
2022-10-01 16:02:19 -0700

    Compelete sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -13,12 +13,13 @@ import {
 } from "@material-ui/icons";
 
 import "./Sidebar.css";
+import { Button } from "@material-ui/core";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <Twitter />
-      <SidebarOption text="Home" Icon={Home} />
+      <SidebarOption active text="Home" Icon={Home} />
       <SidebarOption text="Search" Icon={Search} />
       <SidebarOption text="Notifications" Icon={NotificationsNone} />
       <SidebarOption text="Messages" Icon={MailOutline} />
@@ -26,6 +27,10 @@ const Sidebar = () => {
       <SidebarOption text="Lists" Icon={ListAlt} />
       <SidebarOption text="Profile" Icon={PermIdentity} />
       <SidebarOption text="More" Icon={MoreHoriz} />
+
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
     </div>
   );
 };
